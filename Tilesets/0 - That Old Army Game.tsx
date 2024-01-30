3d2ad267321ce33705eac3621755e8a9d4246640
Enmet <enberg.metal@gmail.com>
2024-01-31 00:21:30 +0100

    Delete Tilesets/0 - That Old Army Game.tsx

--- Tilesets/0 - That Old Army Game.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="0 - That Old Army Game" tilewidth="32" tileheight="32" tilecount="256" columns="16">
- <image source="0 - That Old Army Game.png" trans="4f788c" width="512" height="512"/>
-</tileset>
